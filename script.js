1971dc030c700a373b68e56fe57e06c94d3d96db
ashishverma1999 <146584123+ashishverma1999@users.noreply.github.com>
2024-01-08 21:30:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+let myScore = document.getElementById("myscore").innerHTML;
+let comp = document.getElementById("comp").innerHTML;
+
+function myFun1(){
+    let a = Math.random();
+let b = a*3;
+let c = Math.floor(b)
+console.log(c);
+    let rocks = document.getElementById("userImg").src="rock.png";
+
+    if(c == 0){
+        let rock = document.getElementById("compImg").src="rock.png";
+        if(rocks && rock)
+        document.getElementById("result").innerHTML="Match Draw"
+    }
+    if(c == 1){
+        let paper = document.getElementById("compImg").src="paper.png";
+        if(rocks && paper)
+        document.getElementById("result").innerHTML="You Loss"
+        comp++;
+        document.getElementById("comp").innerHTML=comp;
+    }
+    if(c == 2){
+        let scissor = document.getElementById("compImg").src="scissor.png";
+        if(rocks && scissor)
+        document.getElementById("result").innerHTML="You Won "
+        myScore++;
+        document.getElementById("myscore").innerHTML=myScore;
+    }
+}
+
+
+function myFun2(){
+    let a = Math.random();
+let b = a*3;
+let c = Math.floor(b)
+console.log(c);
+    let papers = document.getElementById("userImg").src="paper.png";
+
+    if(c == 0){
+        let rock = document.getElementById("compImg").src="rock.png";
+        if(papers && rock)
+        document.getElementById("result").innerHTML="You Won"
+        myScore++;
+        document.getElementById("myscore").innerHTML=myScore;
+    }
+    if(c == 1){
+        let paper = document.getElementById("compImg").src="paper.png";
+        if(papers && paper)
+        document.getElementById("result").innerHTML="Match Draw"
+    }
+    if(c == 2){
+        let scissor = document.getElementById("compImg").src="scissor.png";
+        if(papers && scissor)
+        document.getElementById("result").innerHTML="You Loss "
+        comp++;
+        document.getElementById("comp").innerHTML=comp;
+    }
+}
+    
+
+function myFun3(){
+    let a = Math.random();
+let b = a*3;
+let c = Math.floor(b)
+console.log(c);
+    let scissors = document.getElementById("userImg").src="scissor.png";
+    if(c == 0){
+        let rock = document.getElementById("compImg").src="rock.png";
+        if(scissors && rock)
+        document.getElementById("result").innerHTML="You Loss"
+        comp++;
+        document.getElementById("comp").innerHTML=comp;
+    }
+    if(c == 1){
+        let paper = document.getElementById("compImg").src="paper.png";
+        if(scissors && paper)
+        document.getElementById("result").innerHTML="You Won"
+        myScore++;
+        document.getElementById("myscore").innerHTML=myScore;
+    }
+    if(c == 2){
+        let scissor = document.getElementById("compImg").src="scissor.png";
+        if(scissors && scissor)
+        document.getElementById("result").innerHTML="Match Draw "
+    }
+    
+}
